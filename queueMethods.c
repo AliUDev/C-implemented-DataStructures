5e92b088ed202059d2be58426514c44d48968c10
Ali Umar <ali.umar.work@gmail.com>
2019-08-27 18:23:36 -0400

    Update queueMethods.c
    creating a specific linked list implementation for my Queue

--- queueMethods.c
+++ queueMethods.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "queue.h"
-
+void initList(struct List *list){
+    list->head = NULL;
+    list->size = 0;
+}
 void insertStart(struct List *list, int x){
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
 
@@ -14,6 +17,7 @@ void insertStart(struct List *list, int x){
 
     list->size++;
 }
+
 int popFirst(struct List *list){
     int retVal;
     struct Node *temp = list->head;
@@ -24,29 +28,25 @@ int popFirst(struct List *list){
     free(temp);
     return retVal;
 }
-void destroyList(struct List *list){
 
+void destroyList(struct List *list){
+    struct Node *temp = list->head;
+
+    printf("Destroying list...");
+    while(list->head != NULL){
+        temp = list->head;
+        list->head = list->head->next;
+        free(temp);
+    }
 }
 
-void initQ(queue *q, unsigned int size){
-    q->storage = (int *)malloc(size * sizeof(int));
+void initQ(struct Queue *q){
+    q->storage = (struct List*)malloc(sizeof(struct List));
+    initList(q->storage);
     q->size = 0;
 }
-
-void insert(queue *q, int x){
-
-}
-
-int popQ(queue *q){
-
-}
-
-int peekQ(queue *q){
-}
-unsigned int getSize(queue *q){
-
-}
-
-unsigned int isEmpty(queue *q){
-
-}
+void insert(struct Queue *q, int x);
+int popQ(struct Queue *q);
+int peekQ(struct Queue *q);
+unsigned int getSize(struct Queue *q);
+unsigned int isEmpty(struct Queue *q);
